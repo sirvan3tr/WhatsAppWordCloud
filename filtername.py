bb4a59d22253d58b05161a7fc98c7a99023d00ad
sirvan3tr <sirvan3tr@gmail.com>
2019-08-05 18:40:05 +0100

    first commit

---
+++ filtername.py
@@ -0,0 +1,34 @@
+#Extremely simple script to cleanup the conversation data that WhatsApp generates
+#You can email yourself the conversation from your WhatsApp mobile application, run this script and use any popular word cloud generator like http://www.wordle.net/create.
+#TO DO: Include code to create wordcloud as well. Possibly https://github.com/atizo/PyTagCloud or https://github.com/amueller/word_cloud
+#Usage: python WhatsAppCleaner.py Foo.txt
+
+from sys import argv
+script, filename, name = argv
+arr = []
+f = open(filename,'r')
+for line in f:
+ try:
+  temp = line.split(': ')[1];
+  temp0 = line.split(': ')[0];
+  if('Media' not in temp and name in temp0): # If any form of media is sent, WhatsApp prints "<Media Omitted>". Getting rid of this. TO DO: Improve this functionality. 
+   arr.append(temp)
+ except:
+     print " "
+f.close()
+
+
+f= open("whatsappchat.txt","w+")
+#f=open("guru99.txt","a+")
+for i in arr:
+    f.write(i)
+f.close()   
+#Open the file back and read the contents
+#f=open("guru99.txt", "r")
+#   if f.mode == 'r': 
+#     contents =f.read()
+#     print contents
+#or, readlines reads the individual line into a list
+#fl =f.readlines()
+#for x in fl:
+#print x
